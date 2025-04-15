6da85cbddca872ef0d793a332dcea1151ba04dd2
MandePriscille <mandepriscille1@gmail.com>
2025-04-15 15:12:50 +0100

    implementation model user

--- authentification/user/models.py
+++ authentification/user/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager,AbstractBaseUser
+from django.utils import timezone
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        return self.create_user(email, password, **extra_fields)
+    
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(BaseModel, AbstractBaseUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, unique=True)
+    is_active = models.BooleanField(default=True)
+    phone_number = models.CharField(max_length=20)
+    username= None
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
+
+class Otp(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=20)
+    expiration_at = models.DateField()
+    is_used = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.email}'s OTP"
+    
+    def is_valid(self):
+        return not self.is_used and timezone.now() < self.expiration_at
